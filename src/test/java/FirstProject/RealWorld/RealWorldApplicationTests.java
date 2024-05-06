c3cf648a1d3b4e6242f98b796968d9f1efc14313
Igor T <postaff12@gmail.com>
2024-05-06 12:52:49 +0300

    start new project

---
+++ src/test/java/FirstProject/RealWorld/RealWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package FirstProject.RealWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
