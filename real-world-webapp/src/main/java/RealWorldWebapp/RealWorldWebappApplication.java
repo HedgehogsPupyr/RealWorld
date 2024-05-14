29dfbc468d8ef1bad63711059a400ff6f5755e5c
Igor T <postaff12@gmail.com>
2024-05-14 16:57:16 +0300

    Приложение запускается

---
+++ real-world-webapp/src/main/java/RealWorldWebapp/RealWorldWebappApplication.java
@@ -0,0 +1,13 @@
+package RealWorldWebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealWorldWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealWorldWebappApplication.class, args);
+
+    }
+}
