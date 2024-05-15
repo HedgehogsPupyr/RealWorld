cfdf9ccad63f94a4d6725486fc06d01d3479063b
HedgehogsPupyr <127382037+HedgehogsPupyr@users.noreply.github.com>
2024-05-15 10:31:12 +0300

    Add sub modules (#2)
    Divide application into several modules

---
+++ real-world-webapp/src/main/java/com/real/world/webapp/RealWorldWebappApplication.java
@@ -0,0 +1,13 @@
+package com.real.world.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealWorldWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealWorldWebappApplication.class, args);
+
+    }
+}
